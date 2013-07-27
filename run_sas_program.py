8b6b1fd8491dac89b9f1df11e1e051c0f0e12fcb
Roy Pardee <rpardee@gmail.com>
2013-07-27 09:46:06 -0700

    Made the error detection regex in run_sas_program.py and show_next_error.py both feed off a user setting.

--- run_sas_program.py
+++ run_sas_program.py
@@ -15,29 +15,34 @@ class RunSasProgramCommand(sublime_plugin.WindowCommand):
       s = sublime.load_settings('sas.sublime-settings')
       sas_path = s.get('sas-path', "C:\\Program Files\\SAS\\SASFoundation\\9.2\\sas.exe")
       sas_args = s.get('sas-args', ['-nologo', '-noovp'])
-      err_regx = re.compile(s.get('err-regx', "(^(error|warning:)|uninitialized|[^l]remerge|Invalid data for)(?! (the .{4,15} product with which|your system is scheduled|will be expiring soon, and|this upcoming expiration.|information on your warning period.))"), re.MULTILINE)
+      err_regx = s.get('err-regx', "(^(error|warning:)|uninitialized|[^l]remerge|Invalid data for)(?! (the .{4,15} product with which|your system is scheduled|will be expiring soon, and|this upcoming expiration.|information on your warning period.))")
       s.set('sas-path', sas_path)
       s.set('sas-args', sas_args)
+      s.set('err-regx', err_regx)
       sublime.save_settings('sas.sublime-settings')
-      call_args = [sas_path, '-sysin', prg_filename, '-log', log_filename, '-print', lst_filename] + sas_args
-      # print subprocess.list2cmdline(call_args)
-      subprocess.call(call_args)
-      sublime.status_message("Finished running " + prg_filename)
-      if os.path.exists(lst_filename):
-        self.window.open_file(lst_filename)
-      if os.path.exists(log_filename):
-        log = open(log_filename)
-        log_contents = log.read()
-        log.close()
-        num_errs = len(re.findall(err_regx, log_contents))
-        sublime.message_dialog("Finished!  There were " + str(num_errs) + " errors.")
-        self.window.open_file(log_filename)
-        self.window.active_view().run_command('show_next_error')
+      err_regx = re.compile(err_regx, re.MULTILINE)
+      if os.path.exists(sas_path):
+        call_args = [sas_path, '-sysin', prg_filename, '-log', log_filename, '-print', lst_filename] + sas_args
+        # print subprocess.list2cmdline(call_args)
+        subprocess.call(call_args)
+        sublime.status_message("Finished running " + prg_filename)
+        if os.path.exists(lst_filename):
+          self.window.open_file(lst_filename)
+        if os.path.exists(log_filename):
+          log = open(log_filename)
+          log_contents = log.read()
+          log.close()
+          num_errs = len(re.findall(err_regx, log_contents))
+          sublime.message_dialog("Finished!  There were " + str(num_errs) + " errors.")
+          self.window.open_file(log_filename)
+          self.window.active_view().run_command('show_next_error')
+        else:
+          sublime.message_dialog("Problem!  Did not find the expected log file (" + log_filename + ").")
+        # print sas_path + " exists?: " + str(os.path.exists(sas_path))
+        # sublime.message_dialog("Pretend I ran " + sas_path)
+        # self.window.open_file(r'C:\Users\Roy\AppData\Roaming\Sublime Text 3\Packages\SAS\notes.txt')
       else:
-        sublime.message_dialog("Problem!  Did not find the expected log file (" + log_filename + ").")
-      # print sas_path + " exists?: " + str(os.path.exists(sas_path))
-      # sublime.message_dialog("Pretend I ran " + sas_path)
-      # self.window.open_file(r'C:\Users\Roy\AppData\Roaming\Sublime Text 3\Packages\SAS\notes.txt')
+        sublime.message_dialog("Problem--could not find sas.exe at " + sas_path + ".  Please update the sas-path setting in sas.sublime-settings in the User package folder.")
     else:
       sublime.message_dialog('Sorry--this only works with .sas files.')
 
--- show_next_error.py
+++ show_next_error.py
@@ -1,15 +1,19 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, re
 
 class ShowNextErrorCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    err_regex = "(\n(error|warning:)|uninitialized|[^l]remerge|Invalid data for)(?! (the .{4,15} product with which|your system is scheduled|will be expiring soon, and|this upcoming expiration.|information on your warning period.))"
+    s = sublime.load_settings('sas.sublime-settings')
+    err_regx = s.get('err-regx', "(^(error|warning:)|uninitialized|[^l]remerge|Invalid data for)(?! (the .{4,15} product with which|your system is scheduled|will be expiring soon, and|this upcoming expiration.|information on your warning period.))")
+    s.set('err-regx', err_regx)
+    sublime.save_settings('sas.sublime-settings')
+    # err_regx = re.compile(err_regx, re.MULTILINE)
     # Get end of last current selection.
     curr_pos = 0
     for region in self.view.sel():
         curr_pos = region.end()
 
     # Find the next error
-    next_error = self.view.find(err_regex, curr_pos, sublime.IGNORECASE)
+    next_error = self.view.find(err_regx, curr_pos, sublime.IGNORECASE)
     if next_error:
       # Clear out any previous selections.
       self.view.sel().clear()
